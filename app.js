0dc36685b39ea15966de455bf94444166ed35bab
yewon-Noh <yewo2nn16@gmail.com>
2022-04-02 03:48:40 +0900

    0402_api(1)
    로그인, 회원가입, 비밀번호, 홈 틀 작성

--- app.js
+++ app.js
@@ -1,15 +1,110 @@
-const express = require('express')
+const express = require('express');
+const path = require("path");
+const cors = require('cors');
+const bodyParser = require('body-parser');
 
-const app = express()
+const app = express();
 
-const PORT = 5000
+const PORT = 5000;
 
-app.set("view engine", "ejs")
-app.set("views", __dirname + "/views")
+// cors
+app.use(cors());
 
-app.use("/public", express.static(__dirname + "/public"))
+// view
+app.set("view engine", "ejs");
+app.set("views", __dirname + "/views");
 
-app.get("/", (req, res) => res.render("home"))
+// body parser
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-const handleListen = () => console.log(`Listening on http://localhost:${PORT}`)
-app.listen(PORT, handleListen)
+// dircetory
+app.use("/public", express.static(__dirname + "/public"));
+// app.use(express.static(path.join(__dirname, 'react/build')));
+
+// get
+app.get("/", (req, res) => res.render("home"));
+
+// post
+app.post("/login", (req, res) => {
+    const account = req.body.params;
+    const email = account.email;
+    const password = account.password;
+
+    // login check
+    if(email == "test@test.com" && password == "1234"){
+        res.send({"result": "ok"});
+    } else {
+        res.send({"result": "fail"});
+    }
+})
+
+app.post("/join", (req, res) => {
+    const account = req.body.params;
+    const name = account.name;
+    const email = account.email;
+    const password = account.password;
+    const tel = account.tel;
+    const agree = account.agree;
+
+    // Join
+    if(name == "test"){
+        res.send({"result": "ok"});
+    } else {
+        res.send({"result": "fail"});
+    }
+})
+
+app.post("/account/password", (req, res) => {
+    const account = req.body.params;
+    const name = account.name;
+    const email = account.email;
+
+    // account check
+
+    // create temporary password
+    let new_password;
+    new_password = "1111";
+
+    // send email
+    let sendEmail = false;
+
+    // return 
+    if(new_password != null && sendEmail){
+        res.send({"result": "ok"});
+    } else {
+        res.send({"result": "fail"});
+    }
+})
+
+app.post("/home", (req, res) => {
+    const id = req.body.params.id;
+
+    // search server list
+    // return srv_id, srv_name, (next)calendar_date, (next)calendar_time, memberList_attendRate
+    let srv_id;
+    let srv_name;
+    let next_meeting;
+    let attend_rate;
+
+    if(TRUE){
+        res.send({
+            "result":"ok",
+            "srv_id": srv_id,
+            "srv_name": srv_name,
+            "next_meeting": next_meeting,
+            "attend_rate": attend_rate
+        })
+    } else {
+        res.send({
+            "result": "fail" 
+        })
+    }
+})
+
+
+// express server listen
+const handleListen = () => 
+    console.log(`Listening on http://localhost:${PORT}`);
+
+app.listen(PORT, handleListen);
