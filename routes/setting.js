8bec473a17963824cc14973b7a37cf8f5516dd1d
yewon-Noh <yewo2nn16@gmail.com>
2022-04-26 01:36:40 +0900

    0426_setting 이름, 상태메시지 변경

--- routes/setting.js
+++ routes/setting.js
@@ -6,14 +6,17 @@
 // @date : 2022-04-26
 // @modifier : 노예원
 // @did
-//  - 프로필 표시
-// @todo
 //  - 이름 변경
 //  - 상태메시지 변경
+// @todo
+//  - 
 // ***********************************************************
 
 const router = require('express').Router();
 const DB = require('../models/config');
+
+// test
+router.get('/set', (req, res) => res.render("setting"))
 
 router.get('/', (req, res) => {
     const user_id = req.user;
@@ -26,7 +29,6 @@ router.get('/', (req, res) => {
     } else {
 
         let sql = 'SELECT user_name, user_msg FROM user WHERE user_id=(?) and user_YN  = \'N\''
-
         let params = [user_id];
         DB(sql, params).then(function (result) {
 
@@ -49,20 +51,79 @@ router.get('/', (req, res) => {
     }
 })
 
-
 router.post('/name', (req, res) => {
     const user_id = req.user;
 
     if(!user_id) {
+        res.send({
+            "result": -1,
+            "msg": "이름이 변경되지않았습니다.",
+        })
 
     } else {
         const account = req.body;
         const name = account.name;
 
-        console.log(user_id, name)
+        // console.log(user_id, name)
+        let sql = 'UPDATE user SET user_name=? WHERE user_id=?'
+        let params = [name, user_id];
+        DB(sql, params).then(function (result) {
+
+            // return 
+            if (!result.state) {
+                console.log(result.err);
+                res.send({
+                    "result": -1,
+                    "msg": "이름이 변경되지않았습니다.",
+                })
+
+            } else {
+                res.send({
+                    "result": 1,
+                    "msg": "이름이 변경되었습니다.",
+                })
+            }
+        })
+
     }
     
-    res.send('a')
+})
+
+router.post('/msg', (req, res) => {
+    const user_id = req.user;
+
+    if(!user_id) {
+        res.send({
+            "result": -1,
+            "msg": "상태메시지가 변경되지않았습니다.",
+        })
+
+    } else {
+        const account = req.body;
+        const msg = account.msg;
+
+        let sql = 'UPDATE user SET user_msg=? WHERE user_id=?'
+        let params = [msg, user_id];
+        DB(sql, params).then(function (result) {
+
+            // return 
+            if (!result.state) {
+                console.log(result.err);
+                res.send({
+                    "result": -1,
+                    "msg": "상태메시지가 변경되지않았습니다.",
+                })
+
+            } else {
+                res.send({
+                    "result": 1,
+                    "msg": "상태메시지가 변경되었습니다.",
+                })
+            }
+        })
+
+    }
+    
 })
 
 module.exports = router;
