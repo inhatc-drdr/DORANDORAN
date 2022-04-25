65606c3ee9002ea1ead7a35514da7f803c9cb75c
yewon-Noh <yewo2nn16@gmail.com>
2022-04-26 00:45:23 +0900

    0426_home 수정
    - 일정 id는 가져오지 않도록 수정

--- routes/home.js
+++ routes/home.js
@@ -1,12 +1,12 @@
 // ***********************************************************
 // HOME API
 // ***********************************************************
-// @description : 홈 화면 관련 라우터
-//  - 서버 목록, 서버 추가
-// @date : 2022-04-25
+// @description : 홈 화면
+//  - 서버 목록
+// @date : 2022-04-26
 // @modifier : 노예원
 // @did
-//  - 
+//  - 서버 목록
 // @todo
 //  - 
 // ***********************************************************
@@ -34,7 +34,7 @@ router.get('/', (req, res) => {
         // - 최근 접속 순
 
         let sql = 'SELECT s.srv_id, s.srv_name, '
-            + '(SELECT calendar_id FROM calendar c WHERE c.srv_id=s.srv_id and calendar_YN = \'N\' and calendar_date >= now() order by calendar_date asc limit 1) as calendar_id, ' 
+            // + '(SELECT calendar_id FROM calendar c WHERE c.srv_id=s.srv_id and calendar_YN = \'N\' and calendar_date >= now() order by calendar_date asc limit 1) as calendar_id, ' 
             + '(SELECT calendar_date FROM calendar c WHERE c.srv_id=s.srv_id and calendar_YN = \'N\' and calendar_date >= now() order by calendar_date asc limit 1) as calendar_date '
             + 'FROM srvuser su, srv s '
             + 'WHERE su.user_id=(?) and su.srv_id = s.srv_id and su.srvuser_YN = \'N\' '
