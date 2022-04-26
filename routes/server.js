b14b7273e6684e29f72cfbeeb59e933fb8b6aef1
yewon-Noh <yewo2nn16@gmail.com>
2022-04-26 18:01:28 +0900

    0426_server 접속

--- routes/server.js
+++ routes/server.js
@@ -1,12 +1,12 @@
 // ***********************************************************
 // SERVER API
 // ***********************************************************
-// @description : 서버 관련 라우터
-//  - 서버 생성, 공지, 일정, ... 
+// @description : 서버 기능
+//  - 서버 접속, 공지, 일정 등 확인 
 // @date : 2022-04-26
 // @modifier : 노예원
 // @did
-//  - 서버 추가
+//  - 서버 접속
 // @todo
 //  - 
 // ***********************************************************
@@ -15,75 +15,80 @@ const router = require('express').Router();
 const DB = require('../models/config');
 
 //test
-router.get('/add', (req, res) => res.render('server'))
+router.get('/test', (req, res) => {
+    res.send(req.session.sid)
+})
 
-router.post('/add', (req, res) => {
-
+// 서버 접속
+// 멤버 여부 확인 -> 접속 시간 저장 -> 세션 저장
+router.get('/', (req, res) => {
     const user_id = req.user;
 
     if (!user_id) {
-        res.send({ 
+        res.send({
             "result": 0,
             "msg": "로그인 되어있지않습니다.",
         });
 
     } else {
-        const server = req.body;
-        const srv_name = server.srv_name;
 
-        // 서버 추가
-        let sql = 'INSERT INTO srv (srv_name, user_id) VALUES(?,?)';
-        let params = [srv_name, user_id];
+        const server = req.query;
+        const srv_id = server.srv_id;
+
+        // 서버의 멤버인지 확인
+        let sql = 'SELECT srvuser_id FROM srvuser WHERE srv_id=? AND user_id=? AND srvuser_YN=\'N\'';
+        let params = [srv_id, user_id];
         DB(sql, params).then(function (result) {
 
             if (!result.state) {
                 console.log(result.err);
-                res.send({ 
+                res.send({
                     "result": -1,
                 });
 
             } else {
 
-                // 관리자를 서버 회원 목록에 추가
-                sql = 'INSERT INTO srvuser (srv_id, user_id) VALUES('
-                    + ' (SELECT srv_id FROM srv WHERE srv_name=? and user_id=?), ?)';
-                params = [srv_name, user_id, user_id];
-                DB(sql, params).then(function (result) {
+                if (!result.rows[0]) {
+                    res.send({
+                        "result": -1,
+                    });
 
-                    if (!result.state) {
-                        console.log(result.err);
+                } else {
+                    const srvuser_id = result.rows[0].srvuser_id;
 
-                        // 실패시 추가된 서버도 삭제
-                        sql = 'UPDATE srv SET srv_YN=\'Y\', srv_delete=CURRENT_TIMESTAMP WHERE srv_id = (SELECT srv_id FROM srv WHERE srv_name=? and user_id=?)';
-                        params = [srv_name, user_id];
-                        DB(sql, params).then(function (result) {
+                    // 접속 시간 저장
+                    sql = 'UPDATE srvuser SET srvuser_lastaccess=CURRENT_TIMESTAMP WHERE srvuser_id=?';
+                    params = [srvuser_id];
+                    DB(sql, params).then(function (result) {
 
-                            if (!result.state) {
-                                console.log(result.err);
-                                res.send({ 
-                                    "result": -1,
-                                });
+                        if (!result.state) {
+                            console.log(result.err);
+                            res.send({
+                                "result": -1,
+                            });
 
-                            } else {
-                                res.send({ 
-                                    "result": -1,
-                                });
-                            }
-                        })
+                        } else {
 
-                    } else {
-                        res.send({ 
-                            "result": 1,
-                            "msg": "서버가 생성되었습니다."
-                         });
-                    }
-                })
+                            // 접속한 서버 정보 세션 저장
+                            req.session.sid = srv_id;
+                            req.session.save(err => {
+                                if (err) {
+                                    console.log(err);
+                                    return res.status(500).send("<h1>500 error</h1>");
+                                }
 
+                                res.send({
+                                    "result": 1,
+                                })
+                            })
+
+                        }
+                    })
+                }
             }
         })
     }
 })
-
 
 
 module.exports = router;
