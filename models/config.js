379006cc66b1a493e857f0fb270aa9e7e4d56c7c
yewon-Noh <yewo2nn16@gmail.com>
2022-04-26 09:37:18 +0900

    0426_mysql datetime 설정

--- models/config.js
+++ models/config.js
@@ -5,7 +5,8 @@ const info = {
     host: process.env.DB_HOST,    
     user: process.env.DB_USER,          
     password: process.env.DB_PASS,      
-    database: process.env.DB_NAME       
+    database: process.env.DB_NAME,
+    dateStrings: 'date',       
 };
 
 const pool = mysql.createPool(info);
