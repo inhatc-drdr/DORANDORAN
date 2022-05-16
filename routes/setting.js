86ebb2d94845534b36a484657af49d227fabb943
yewon-Noh <yewo2nn16@gmail.com>
2022-05-16 11:37:31 +0900

    0516_비밀번호 변경

--- routes/setting.js
+++ routes/setting.js
@@ -2,12 +2,11 @@
 // SETTING API
 // ***********************************************************
 // @description : 설정 화면
-//  - 프로필 설정
+//  - 프로필 설정, 비밀번호 변경
 // @date : 2022-04-26
 // @modifier : 노예원
 // @did
-//  - 이름 변경
-//  - 상태메시지 변경
+//  - 비밀번호 변경
 // @todo
 //  -
 // ***********************************************************
@@ -15,6 +14,7 @@
 const router = require("express").Router();
 const DB = require("../models/config");
 const { resultMSG, resultList } = require("./send");
+const { hashCreate, hashCheck } = require("../config/crypto");
 
 router.get("/", (req, res) => {
 
@@ -23,7 +23,7 @@ router.get("/", (req, res) => {
   console.log(`[${new Date().toLocaleString()}] [uid ${user_id} /setting] `);
 
   let sql =
-    "SELECT user_name, user_msg FROM user WHERE user_id=(?) and user_YN  = 'N'";
+    "SELECT user_email, user_name FROM user WHERE user_id=(?) and user_YN  = 'N'";
   let params = [user_id];
   DB(sql, params).then(function (result) {
     // return
@@ -37,10 +37,6 @@ router.get("/", (req, res) => {
       }
 
       resultList(res, 1, null, result.rows);
-      // res.send({
-      //   result: 1,
-      //   list: result.rows,
-      // });
     }
   });
 });
@@ -86,5 +82,74 @@ router.post("/msg", (req, res) => {
     }
   });
 });
+
+// 비밀번호 변경
+router.post("/changePassword", (req, res) => {
+  const user_id = req.user.id;
+  let pwd = req.body.pwd;
+  let changePwd = req.body.changePwd;
+
+  console.log(
+    `[${new Date().toLocaleString()}] [uid ${user_id} /setting/changePassword] pwd=${pwd}&changePwd=${changePwd}`
+  );
+
+
+  if (!pwd || !changePwd) {
+    return resultMSG(res, -1, "모두 입력되지 않았습니다.");
+
+  } else {
+    // login check
+    let sql = 'SELECT count(*) as count, user_pwd, user_salt FROM user WHERE user_id=? AND user_YN=\'N\'';
+    let params = [user_id];
+    DB(sql, params).then((result) => {
+
+      if (!result.state) {
+        console.log(result.err);
+
+      } else {
+        if (!result.rows[0]) {
+
+        } else {
+          const user_pwd = result.rows[0].user_pwd;
+          const user_salt = result.rows[0].user_salt;
+
+          if (!user_pwd || !user_salt) {
+            return resultMSG(res, -1, "이메일 또는 비밀번호가 일치하지 않습니다.");
+
+          } else {
+
+            // 복호화
+            const crypto_pwd = hashCheck(user_salt, pwd);
+
+            if (user_pwd == crypto_pwd) {
+
+              // 암호화
+              const hashed = hashCreate(changePwd);
+
+              const salt = hashed.salt;
+              const crypto_pwd = hashed.pwd;
+
+              let sql =
+                "UPDATE user SET user_pwd=?, user_salt=? WHERE user_id=? AND user_YN=\'N\'";
+              let params = [crypto_pwd, salt, user_id];
+              DB(sql, params).then(function (result) {
+                // return
+                if (!result.state) {
+                  console.log(result.err);
+                  resultMSG(res, -1, "오류가 발생하였습니다.");
+                } else {
+                  resultMSG(res, 1, "비밀번호가 변경되었습니다.");
+                }
+              });
+
+            } else {
+              return resultMSG(res, -1, "기존 비밀번호가 일치하지 않습니다.");
+            }
+          }
+        }
+      }
+    })
+  }
+})
 
 module.exports = router;
